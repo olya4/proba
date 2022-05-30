ce102e38d2263731d4179e41c4b2385df2397ff7
olya_marusik <o.a.marusik@mail.ru>
2022-05-30 18:14:51 +0300

    add test

---
+++ test/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 27;
+        int z = 14;
+
+        int sum = x + y + z;
+
+        System.out.println(sum);
+    }
+}
